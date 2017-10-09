b8460a3cca8a759e09d9ab2abfb1e26d1848f1dc
marcinN90 <90.marcin.nowak@gmail.com>
2017-10-10 00:22:34 +0200

    Entities: Category and Entry

---
+++ TutoCMS/Models/Category.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TutoCMS.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public List<Entry> Entries { get; set; }
+    }
+}
---
+++ TutoCMS/Models/Entry.cs
@@ -0,0 +1,11 @@
+﻿namespace TutoCMS.Models
+{
+    public class Entry
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+    }
+}
