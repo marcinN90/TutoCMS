cb710dd1751b0de5b5264f71d9eeaaca6a099221
marcinN90 <90.marcin.nowak@gmail.com>
2017-10-16 17:00:09 +0200

    Delete old project

--- Tuto/Models/Entry.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Tuto.Models
-{
-    public class Entry
-    {
-        public int Id { get; set; }
-        public string Title { get; set; }
-        public string Content { get; set; }    
-    }
-}
