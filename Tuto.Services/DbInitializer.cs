23bf8d36d166483f17e3043724558f255bcab840
marcinn90 <90.marcin.nowak@gmail.com>
2017-11-06 00:58:56 +0100

    InMemoryDb

--- Tuto.Services/DbInitializer.cs
+++ Tuto.Services/DbInitializer.cs
@@ -7,10 +7,17 @@ using TutoDataRepo;
 
 namespace TutoRepo
 {
-    #region FakeData
-    public class FakeData
+    using System;
+    using System.Collections.Generic;
+    using Tuto.Data;
+    using Tuto.Data.Models;
+
+    namespace TutoRepo
     {
-        public static string LoremIpsum = @"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras congue convallis urna, in mattis nisl convallis quis. 
+        #region FakeData
+        public class FakeData
+        {
+            public static string LoremIpsum = @"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras congue convallis urna, in mattis nisl convallis quis. 
                 Sed fringilla, odio et convallis lobortis, velit nulla ullamcorper purus, at bibendum eros sapien at ligula. Maecenas a dolor lectus. 
                 Nulla iaculis neque sed ornare sollicitudin. Vestibulum at lobortis nisl. Integer justo metus, bibendum eget tincidunt non, fringilla at libero. 
                 Aliquam tincidunt nulla vitae commodo molestie. Ut vitae finibus orci, ac lobortis tortor. 
@@ -26,7 +33,7 @@ M               auris eget nisl nec massa cursus facilisis eget et ipsum.Nunc ve
                 Aliquam eu mi dolor. In finibus rhoncus fringilla. Morbi commodo nulla vitae lobortis congue. 
                 Sed dui tellus, auctor vel est nec, vulputate condimentum felis.Morbi accumsan tempor porttitor.";
 
-        public List<Entry> Entries = new List<Entry>
+            public List<Entry> Entries = new List<Entry>
         {
             new Entry { Id = 1, CategoryId = 1, Title = "Very awesome part 1", Content = LoremIpsum.Substring(0, 300), LastRevisionAt = DateTime.Now },
             new Entry { Id = 2, CategoryId = 1, Title = "Very awesome part 2", Content = LoremIpsum.Substring(0, 300), LastRevisionAt = DateTime.Now },
@@ -38,86 +45,44 @@ M               auris eget nisl nec massa cursus facilisis eget et ipsum.Nunc ve
             new Entry { Id = 8, CategoryId = 2, Title = "Very awesome part 6", Content = LoremIpsum.Substring(0, 300), LastRevisionAt = DateTime.Now },
         };
 
-        public List<Category> Categories = new List<Category>
+            public List<Category> Categories = new List<Category>
         {
              new Category { Id = 1, Title = "Awesome", Description ="This is awesome cateogry for make more awesome courses."},
              new Category  {Id = 2, Title = "Very Awesome",Description ="This is second awesome cateogry for make more awesome courses."}
         };
 
-        public WebsiteDetails WebsiteDetails = new WebsiteDetails
-        {
-            Title = "WebsiteTtile",
-            OwnerEmail = "example@example.com"
-        };
+            public WebsiteDetails WebsiteDetails = new WebsiteDetails
+            {
+                Title = "WebsiteTtile",
+                OwnerEmail = "example@example.com"
+            };
 
-        public HomePageSettings HomePageSettings = new HomePageSettings()
-        {
-            Title = "HomeTitle",
-            SeoDescription = "Awesome SEO Description",
-            Descritpion = LoremIpsum.Substring(0, 500)
-        };
-        public List<Link> Links = new List<Link>
+            public HomePageSettings HomePageSettings = new HomePageSettings()
+            {
+                Title = "HomeTitle",
+                SeoDescription = "Awesome SEO Description",
+                Descritpion = LoremIpsum.Substring(0, 500)
+            };
+            public List<Link> Links = new List<Link>
         {
             new Link { Id = 1, LinkTitle = "Great link to share", UrlAddress = "example.com"},
             new Link { Id = 2, LinkTitle = "Second great link to share", UrlAddress = "example.com2"}
         };
-    }
-    #endregion
+        }
+        #endregion
 
-    public class FakeTutoDataRepo : ITudoDataRepository
-    {
-        FakeData fakeData = new FakeData();
-        public Task<List<Category>> GetAllCategories()
+        public class DbInitializer
         {
-            var categories = fakeData.Categories;
-            foreach (var category in categories)
+            public static void SeeDbWithFakeData(TutoContext context)
             {
-                category.Entries = fakeData.Entries.Where(x => x.CategoryId.Equals(category.Id)).ToList();
+                FakeData fakeData = new FakeData();
+                context.EntryPart.AddRange(fakeData.Entries);
+                context.WebsiteDetails.Add(fakeData.WebsiteDetails);
+                context.HomePageSettings.Add(fakeData.HomePageSettings);
+                context.Category.AddRange(fakeData.Categories);
+                context.Link.AddRange(fakeData.Links);
+                context.SaveChanges();
             }
-            return Task.FromResult(categories.ToList());
         }
-
-        public Task<List<Link>> GetAllLinks()
-        {
-            return Task.FromResult(fakeData.Links);
-        }
-
-        public Category GetCategoryById(int id)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<Entry> GetEntryById(int? id)
-        {
-            var entry = fakeData.Entries
-                .Where(x => x.Id.Equals(id))
-                .FirstOrDefault()
-                ;
-            if (entry == null)
-            {
-                Entry nulledEntry = null;
-                return Task.FromResult(nulledEntry);
-            }
-
-
-            entry.Category = fakeData.Categories.Where(x => x.Id.Equals(entry.CategoryId)).FirstOrDefault();
-            return Task.FromResult(entry);
-        }
-
-        public Task<string> GetGoogleAnalyticsKey()
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<HomePageSettings> GetHomePageSettings()
-        {
-            return Task.FromResult(fakeData.HomePageSettings);
-        }
-
-        public Task<WebsiteDetails> GetWebsiteDetails()
-        {
-            return Task.FromResult(fakeData.WebsiteDetails);
-        }
-
     }
 }
