f53ffef6ece28c3b52ae0f74826710c6739a748a
marcinN90 <90.marcin.nowak@gmail.com>
2017-11-20 11:36:19 +0100

    Implement Auto mapper - entries and links

---
+++ Tuto.UI/AutoMapperProfileConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Tuto.Data.Models;
+using Tuto.UI.Models.DTOModels;
+
+namespace Tuto.UI
+{
+    public class AutoMapperProfileConfiguration : Profile
+    {
+        public AutoMapperProfileConfiguration() : this("MainProfile")
+        {
+        }
+        protected AutoMapperProfileConfiguration (string profileName) : base(profileName)
+        {
+            CreateMap<Link, LinkDTO>();
+            CreateMap<Entry, EntryDTO>();
+        }
+
+    }
+}
